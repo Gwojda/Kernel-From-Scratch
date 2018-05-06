b5761ee18bf8a88d79a888eab80ebef4a4470865
gwojda <gwojda@student.42.fr>
2018-05-06 15:16:18 +0200

    push

--- isofiles/boot/boot.asm
+++ isofiles/boot/boot.asm
@@ -1,8 +1,30 @@
 global start
+extern kmain, start_ctors, end_ctors, start_dtors, end_dtors
 
-section .text
-bits 32
 start:
-    ; print `OK` to screen
-    mov dword [0xb8000], 0x2f4b2f4f
-    hlt
+	push ebx
+
+static_ctors_loop:
+	mov ebx, start_ctors
+	jmp .test
+.body:
+	call [ebx]
+	add ebx,4
+.test:
+	cmp ebx, end_ctors
+	jb .body
+
+call kmain                      ; call kernel proper
+
+static_dtors_loop:
+	mov ebx, start_dtors
+	jmp .test
+.body:
+	call [ebx]
+	add ebx,4
+.test:
+	cmp ebx, end_dtors
+	jb .body
+
+	cli ; stop interrupts
+	hlt ; halt the CPU
