00181d764c04302b153a9bdc125bb044feb0ef5a
gwojda <gwojda@student.42.fr>
2018-05-09 13:40:27 +0000

    prety moon\!

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,26 +2,25 @@
 
 void kmain(void)
 {
-	size_t i = 0;
 	/* Initialize terminal interface */
 	terminal_initialize();
 
 	/* Newline support is left as an exercise. */
 	terminal_color = vga_entry_color(VGA_COLOR_RED, VGA_COLOR_BLACK);
-	terminal_putstr("\
-         ___---___\
-      .--         --.\
-    ./   ()      .-. \\.\
-   /   o    .   (   )  \\\
-  / .            '-'    \\\
- | ()    .  O         .  |\
-|                         |\
-|    o           ()       |\
-|       .--.          O   |\
- | .   |    |            |\
- \\    `.__.'    o   .  /\
-  \\                   /\
-   `\\  o    ()      /'\
-      `--___   ___--'\
-            ---\n");
+	terminal_putstr("\n"
+"         ___---___\n"
+"      .--         --.\n"
+"    ./   ()      .-. \\.\n"
+"   /   o    .   (   )  \\\n"
+"  / .            '-'    \\\n"
+" | ()    .  O         .  |\n"
+"|                         |\n"
+"|    o           ()       |\n"
+"|       .--.          O   |\n"
+" | .   |    |            |\n"
+"  \\    `.__.'    o   .  /\n"
+"   \\                   /\n"
+"    `\\  o    ()      /'\n"
+"      `--___   ___--'\n"
+"            ---\n");
 }
