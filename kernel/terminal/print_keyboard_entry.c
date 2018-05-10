fdbc9e96f9ace73551bcf821e0c0c6b05cfd1d76
gwojda <gwojda@student.42.fr>
2018-05-10 12:21:51 +0200

    start gdb

--- kernel/terminal/print_keyboard_entry.c
+++ kernel/terminal/print_keyboard_entry.c
@@ -47,15 +47,30 @@ void	print_keyboard_entry(void)
 	static char shift_locked = FALSE;
 	unsigned int cr;
 
-	cr = inb(0x64);
-	if (SHIFT_PRESSED(cr))
-		shift_locked = TRUE;
-	if (SHIFT_RELEASED(cr))
-		shift_locked = FALSE;
-//	terminal_putstr(itoa(cr));
-//	cr &= 0x7F;
-	if (shift_locked)
-		terminal_putchar(value_table[cr]);
-	else
-		terminal_putchar(value_table_shift[cr]);
+/*
+* Status Register - PS/2 Controller
+* Bit Meaning
+* 0   Output buffer status (0 = empty, 1 = full) (must be set before attempting to read data from IO port 0x60)
+* 1   Input buffer status (0 = empty, 1 = full) (must be clear before attempting to write data to IO port 0x60 or IO port 0x64)
+* 2   System Flag - Meant to be cleared on reset and set by firmware (via. PS/2 Controller Configuration Byte) if the system passes self tests (POST)
+* 3   Command/data (0 = data written to input buffer is data for PS/2 device, 1 = data written to input buffer is data for PS/2 controller command)
+* 4   Unknown (chipset specific) - May be "keyboard lock" (more likely unused on modern systems)
+* 5   Unknown (chipset specific) - May be "receive time-out" or "second PS/2 port output buffer full"
+* 6   Time-out error (0 = no error, 1 = time-out error)
+* 7   Parity error (0 = no error, 1 = parity error)
+*/
+	if (inb(0x64) & 1)
+	{
+		cr = inb(0x60);
+		if (SHIFT_PRESSED(cr))
+			shift_locked = TRUE;
+		if (SHIFT_RELEASED(cr))
+			shift_locked = FALSE;
+	//	terminal_putstr(itoa(cr));
+	//	cr &= 0x7F;
+		if (shift_locked)
+			terminal_putchar(value_table[cr]);
+		else
+			terminal_putchar(value_table_shift[cr]);
+	}
 }
