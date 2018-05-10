4587f1530ff8270be4e69eb618aa2867520ec100
gwojda <gwojda@student.42.fr>
2018-05-10 12:39:05 +0000

    cursor now moving \! Not well, but he did \!

--- kernel/terminal/terminal_putentryat.c
+++ kernel/terminal/terminal_putentryat.c
@@ -8,7 +8,7 @@ void	terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 
 	//moving cursor
 	outb(0x3D4, 0x0F);
-	outb(0x3D5, (uint8_t) (pos & 0xFF));
+	outb(0x3D5, (uint8_t) (index & 0xFF));
 	outb(0x3D4, 0x0E);
-	outb(0x3D5, (uint8_t) ((pos >> 8) & 0xFF));
+	outb(0x3D5, (uint8_t) ((index >> 8) & 0xFF));
 }
