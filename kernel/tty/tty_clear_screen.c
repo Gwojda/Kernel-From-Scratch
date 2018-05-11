55d0da144034d22f0d5cc9cb9fd9f626a0b4f2cd
gwojda <gwojda@student.42.fr>
2018-05-11 12:38:34 +0000

    KFS-1 finished

--- kernel/tty/tty_clear_screen.c
+++ kernel/tty/tty_clear_screen.c
@@ -8,4 +8,11 @@ void	tty_clear_screen(size_t ind)
 		for (size_t x = 0; x < VGA_WIDTH; ++x)
 			tty[ind].screen[(y * VGA_WIDTH) + x] = vga_entry(' ', tty[ind].tty_color);
 	}
+	tty[ind].tty_row = 0;
+	tty[ind].tty_column = 0;
+	if (current_tty == ind)
+	{
+		vga_render_tty();
+		vga_move_cursor(0, 0);
+	}
 }
--- kernel/tty/tty_scrollup.c
+++ kernel/tty/tty_scrollup.c
@@ -9,7 +9,7 @@ void	tty_scrollup(void)
 	memmove((void *)tty[current_tty].screen, (const void *)(tty[current_tty].screen + VGA_WIDTH), VGA_WIDTH * (VGA_HEIGHT - 1) * sizeof(*tty[current_tty].screen));
 	while (i < VGA_WIDTH)
 	{
-		tty[current_tty].screen[VGA_WIDTH * (VGA_HEIGHT - 1) + i] = vga_entry(' ', tty[ind].tty_color);
+		tty[current_tty].screen[VGA_WIDTH * (VGA_HEIGHT - 1) + i] = vga_entry(' ', tty[current_tty].tty_color);
 		++i;
 	}
 }
