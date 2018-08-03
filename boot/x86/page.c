5a30722de62ed2da0b711d201087426a88b2d1a6
root <root@vps367481.ovh.net>
2018-08-03 16:48:49 +0200

    add virtual physical map interface

--- boot/x86/page.c
+++ boot/x86/page.c
@@ -62,33 +62,38 @@ void page_setup_kernel_section(uint32_t *table)
 	page_entry_set_range(table, KERNEL_REAL_BSS_START >> 12, PAGE_ALIGN(KERNEL_REAL_BSS_END) >> 12, PAGE_WRITE | PAGE_PRESENT);
 }
 
-uint32_t *access_table_with_physical(uint32_t *empty_static_page, uint32_t *physical)
+uint32_t *access_table_with_physical(void *empty_static_page, uint32_t *physical)
 {
-	// ??
 	unsigned int i = KERNEL_GET_REAL(empty_static_page);
-	page_entry_set(kernel_page, i & 1024, i & 1024, PAGE_WRITE | PAGE_PRESENT);
+	page_entry_set(kernel_page, i >> 12, physical, PAGE_WRITE | PAGE_PRESENT);
 	return (empty_static_page);
 }
 
-uint32_t page_get_entry_with_virtual(void *addr)
-{
-	unsigned int page_directory_index = (size_t)addr >> 22;
-	unsigned int page_table_index = ((size_t)addr >> 12) & 0x03FF;
-
-	uint32_t *page_directory = KERNEL_GET_VIRTUAL(page_directory_get());
-	uint32_t directory_entry = page_directory[page_directory_index];
-	// not sure for the next condition here, need to ask Nico's help
-	if (directory_entry & PAGE_SIZE)
-		return (directory_entry);
-	uint32_t *page_table = KERNEL_GET_VIRTUAL(directory_entry & PAGE_ADDR);
-	return page_table[page_table_index] & PAGE_ADDR;
-}
-
-/*
 int page_map(void *phy_addr, void *virt_addr, unsigned int flag)
 {
-	
-}*/
+	if ((uint32_t)phy_addr & PAGE_FLAG)
+		return 0;
+	if (flag & PAGE_ADDR)
+		return 0;
+
+	unsigned int page_directory_index = (size_t)virt_addr >> 22;
+	unsigned int page_table_index = ((size_t)virt_addr >> 12) & 0x03FF;
+
+	uint32_t *page_directory = KERNEL_GET_VIRTUAL(page_directory_get());//
+	uint32_t *directory_entry = &(page_directory[page_directory_index]);
+
+	if (*directory_entry & PAGE_SIZE)
+		return 0;
+	if (*directory_entry & PAGE_PRESENT)
+		return 0;
+	if (*directory_entry & PAGE_ADDR)
+		return 0; // allouer
+
+	uint32_t *page_table = access_table_with_physical(page_swap, *directory_entry & PAGE_ADDR);
+	page_directory_reset(); // page update
+
+	page_entry_set(page_table, page_table_index, phy_addr, flag);
+}
 
 
 /*
@@ -121,4 +126,3 @@ void page_setup(void)
 	//access_table_with_physical(page_swap, kernel_page);
 	//printk("page %p\n", page_get_entry_with_virtual(kernel_page));
 }
-
