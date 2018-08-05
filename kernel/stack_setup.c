69465e0fa77906ad0f2bdceb8646b5bb98246572
root <root@vps367481.ovh.net>
2018-08-05 15:34:27 +0200

    better stack init

--- kernel/stack_setup.c
+++ kernel/stack_setup.c
@@ -7,19 +7,22 @@ void	stack_setup(void)
 	void*	virt_stack_ptr = STACK_END - 4096;
 	void	*phys_addr;
 	
-	while (stack_btmp != &stack_bottom/* && virt_stack_ptr > STACK_START*/)
+	while (stack_btmp != &stack_bottom && virt_stack_ptr >= STACK_START)
 	{
-		printk(" %p", virt_stack_ptr);
 		if (!page_map(KERNEL_GET_REAL(stack_btmp), virt_stack_ptr, PAGE_WRITE | PAGE_PRESENT))
 			kern_panic("Stack setup failled\n");
 		virt_stack_ptr -= 4096;
 		stack_btmp -= 4096;
 	}
-/*	while (virt_stack_ptr > STACK_START)
+	if (page_map_at(virt_stack_ptr, PAGE_WRITE | PAGE_PRESENT, (virt_stack_ptr - STACK_START) >> 12) == 0)
+		kern_panic("imposible to map");
+	page_unmap_at(virt_stack_ptr, 0, (virt_stack_ptr - STACK_START) >> 12);
+	/*while (virt_stack_ptr >= STACK_START)
 	{
-		phys_addr = get_phys_block(1);
+		if (!(phys_addr = get_phys_block(1)))
+			kern_panic("Stack setup failled no memory left\n");
 		if (!page_map(phys_addr, virt_stack_ptr, PAGE_WRITE | PAGE_PRESENT))
-			kern_panic("Stack setup failled\n");
+			kern_panic("Stack setup failled page_map\n");
 		virt_stack_ptr -= 4096;
 	}*/
 }
