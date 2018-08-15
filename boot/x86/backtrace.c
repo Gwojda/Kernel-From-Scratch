32a56e3f6112f747b631df93c81ee78fb9ba96ba
gwojda <gwojda@student.42.fr>
2018-08-15 20:44:23 +0200

    secure backtrace if stack is corrupt

--- boot/x86/backtrace.c
+++ boot/x86/backtrace.c
@@ -1,5 +1,6 @@
 #include "backtrace.h"
 #include "printk.h"
+#include "page.h"
 
 void	backtrace_cur(unsigned long max)
 {
@@ -16,11 +17,22 @@ void	backtrace(size_t *ebp, unsigned long max)
 	printk("backtrace\n");
 	while (max > 0 && ebp != NULL)
 	{
+		if (page_is_present(ebp + 1) == 0)
+			goto bad_stack;
+		if (page_is_present((char*)(ebp + 1) + sizeof(ebp) - 1) == 0)
+			goto bad_stack;
 		eip = (void*)ebp[1];
 		printk("  %p\n", eip);
 		if (eip == NULL)
 			break ;
+		if (page_is_present(ebp) == 0)
+			goto bad_stack;
+		if (page_is_present((char*)ebp + sizeof(ebp) - 1) == 0)
+			goto bad_stack;
 		ebp = (size_t*)*ebp;
 		max--;
 	}
+	return;
+bad_stack:
+	printk("  stack corrupt\n");
 }
