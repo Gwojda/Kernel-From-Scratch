86b915c2b25cd51ab68eeb7e1022f334562af05c
gwojda <gwojda@student.42.fr>
2018-08-26 18:50:30 +0200

    compilation fine on debian

--- kernel/gen_makefile.sh
+++ kernel/gen_makefile.sh
@@ -3,12 +3,13 @@ echo "old Makefile still alive : .Makefile.reg";
 mv Makefile .Makefile.reg
 echo "Generating new Makefile";
 echo "OBJS += \\" >> Makefile;
-for f in *.c;
+for f in `find . -name "*.c"`;
 do
-	echo "kernel/$f \\" | rev | sed s/c/o/ | rev >> Makefile;
+	echo "kernel/`echo "$f" | cut -c 3-` \\" | rev | sed s/c/o/ | rev >> Makefile;
 done
-for f in **/*.c;
+for f in  `find . -name "*.asm"`;
 do
-	echo "kernel/$f \\" | rev | sed s/c/o/ | rev >> Makefile;
+	echo "kernel/`echo "$f" | cut -c 3-` \\" | rev | sed s/msa/o/ | rev >> Makefile;
 done
+
 echo "new Makefile generated";
